790d84537127e4c0a4ba8953c2d8f30cdf6c57ef
wahyuwidiantoro83 <wahyuwidiantoro83@gmail.com>
2023-12-28 20:15:26 +0700

    fix: fix some known bugs

--- Controller/promotor.js
+++ Controller/promotor.js
@@ -1,8 +1,8 @@
 const { error, log } = require("console");
 const db = require("../Helper/mysql");
 const { templateRes } = require("../Helper/utilist");
-const { accountdetails, events, auths, categories, cities, tickets, promos } = require("../models")
-const fs = require("fs")
+const { accountdetails, events, auths, categories, cities, tickets, promos } = require("../models");
+const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const { Op } = require("sequelize");
 
@@ -11,31 +11,32 @@ module.exports = {
     try {
       console.log("ashasdbjhads", req.body);
       const result = await auths.findOne({
-        where: { email: req.body.email }, raw: true
-      })
+        where: { email: req.body.email },
+        raw: true,
+      });
       console.log("ini result", result);
       if (result) {
         console.log("yuygefjhsbfdsb");
-        const { id, email, username, role } = result
-        const token = jwt.sign({ id, role, username, email }, process.env.SCRT_KEY)
-        return next(templateRes(201, true, "success login", token, null))
+        const { id, email, username, role } = result;
+        const token = jwt.sign({ id, role, username, email }, process.env.SCRT_KEY);
+        return next(templateRes(201, true, "success login", token, null));
       } else {
-        next(templateRes(400, false, "Error login", null, error.message))
+        next(templateRes(400, false, "Error login", null, error.message));
       }
     } catch (error) {
       console.log(error.message);
-      return next(templateRes(201, true, "success login", null, error.message))
+      return next(templateRes(201, true, "success login", null, error.message));
     }
   },
 
   checkRole: async (req, res, next) => {
     try {
       if (req.token) {
-        const result = jwt.verify(req.token)
+        const result = jwt.verify(req.token);
         console.log("ini adalah result", result);
       }
     } catch (error) {
-      return next(templateRes(500, true, "You dont have Authorization", null, error.message))
+      return next(templateRes(500, true, "You dont have Authorization", null, error.message));
     }
   },
 
@@ -43,10 +44,11 @@ module.exports = {
     try {
       // Menemukan id city sebagai foreignkey dalam tabel event
       const getCityId = await cities.findOne({
-        where: { city: req.body.event.eventCity }, raw: true
-      },)
+        where: { city: req.body.event.eventCity },
+        raw: true,
+      });
 
-      // console.log("req getcity id bro",getCityId);
+      console.log("req getcity id bro", getCityId);
       // console.log("req body bro",req.body);
       // console.log("req userdata bro",req.userData);
       // create event dalam DB
@@ -63,11 +65,13 @@ module.exports = {
         image: req.body.event.fileName,
         idCategory: req.body.event.eventCategoryId,
         idPromotor: req.userData.id,
+      });
 
-      })
-
-      // menemukan id event yang baru dibuat untuk mendapatkan 
-      const getEventId = await events.findOne({ where: { name: req.body.event.eventTitle }, raw: true })
+      // menemukan id event yang baru dibuat untuk mendapatkan
+      const getEventId = await events.findOne({
+        where: { name: req.body.event.eventTitle },
+        raw: true,
+      });
 
       const ticketArray = req.body.ticket.map((val, idx) => {
         return {
@@ -77,43 +81,57 @@ module.exports = {
           stock: val.ticketStock,
           ticketSalesStart: val.ticketSalesStart,
           ticketSalesEnd: val.ticketSalesEnd,
-        }
-      })
+        };
+      });
       // console.log("ticket array", ticketArray);
-      const createTicket = await tickets.bulkCreate(ticketArray)
+      const createTicket = await tickets.bulkCreate(ticketArray);
       // console.log("create ticket", createTicket);
+      console.log("ini req promo");
+      const applyPromoArray = [];
+      if (req.body.promo) {
+        for (let i = 0; i < req.body.promo.length; i++) {
+          for (let j = 0; j < req.body.promo[i].applyTo.length; j++) {
+            let getIdTicket = await tickets.findOne({
+              where: {
+                [Op.and]: [
+                  {
+                    idEvent: getEventId.id,
+                  },
+                  {
+                    level: req.body.promo[i].applyTo[j],
+                  },
+                ],
+              },
+              raw: true,
+            });
 
-      const applyPromoArray = []
-      for (let i = 0; i < req.body.promo.length; i++) {
-        for (let j = 0; j < req.body.promo[i].applyTo.length; j++) {
-
-          let getIdTicket = await tickets.findOne({
-            where: {
-              [Op.and]: [{
-                idEvent: getEventId.id
-              }, {
-                level: req.body.promo[i].applyTo[j]
-              }]
-            }, raw: true
-          })
-
-          applyPromoArray.push({
-            idEvent:getEventId.id,
-            idTicket: getIdTicket.id,
-            promoName: req.body.promo[i].promoName,
-            discountAmount: req.body.promo[i].discountAmount,
-            userLimit: req.body.promo[i].promoLimit,
-            codes:`${getEventId.id}${req.body.promo[i].promoLimit}${getIdTicket.id}`,
-            isDeleted: 0
-          })
+            applyPromoArray.push({
+              idEvent: getEventId.id,
+              idTicket: getIdTicket.id,
+              promoName: req.body.promo[i].promoName,
+              discountAmount: req.body.promo[i].discountAmount,
+              userLimit: req.body.promo[i].promoLimit,
+              codes: `${getEventId.id}${req.body.promo[i].promoLimit}${getIdTicket.id}`,
+              isDeleted: 0,
+            });
+          }
         }
+        // console.log("diluar ni cuy",applyPromoArray);
+        const createPromo = await promos.bulkCreate(applyPromoArray);
       }
-      // console.log("diluar ni cuy",applyPromoArray);
-      const createPromo = await promos.bulkCreate(applyPromoArray)
 
-      next(templateRes(201, true, "create event, ticket, promo success", [createEvent, createTicket], null))
+      next(
+        templateRes(
+          201,
+          true,
+          "create event, ticket, promo success",
+          [createEvent, createTicket],
+          null
+        )
+      );
     } catch (error) {
-      next(templateRes(500, false, "cant publish event", null, error.message))
+      console.log(error);
+      next(templateRes(500, false, "cant publish event", null, error.message));
     }
   },
 
@@ -130,74 +148,80 @@ module.exports = {
       console.log("get promotor event", req.userData);
       const getEvent = await events.findAll({
         where: {
-          idPromotor: req.userData.id
+          idPromotor: req.userData.id,
         },
         include: [
-          { // ini untuk join tabel
+          {
+            // ini untuk join tabel
             model: categories,
             required: true,
-            attributes: ["category"]
+            attributes: ["category"],
           },
           {
             model: cities,
             required: true,
-            attributes: ["city"]
-          }
+            attributes: ["city"],
+          },
         ],
-        raw: true
-      })
+        raw: true,
+      });
       const arrayEvent = getEvent.map((val, idx) => {
         return {
           idEvent: val.id,
           name: val.name,
           category: val["category.category"],
           sold: 0,
-          date: `${new Date(val.startDate).toLocaleDateString("en", { day: "numeric", day: "numeric", month: "short", year: "numeric" })}`,
-          status: "active"
-        }
-      })
+          date: `${new Date(val.startDate).toLocaleDateString("en", {
+            day: "numeric",
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+          })}`,
+          status: "active",
+        };
+      });
       console.log("ini array event", arrayEvent);
-      return next(templateRes(201, true, "get event success", arrayEvent, null))
-
+      return next(templateRes(201, true, "get event success", arrayEvent, null));
     } catch (error) {
-      return next(templateRes(500, false, "get event promotor UNSUCCESSFULL", null, error.message))
+      return next(templateRes(500, false, "get event promotor UNSUCCESSFULL", null, error.message));
     }
   },
 
-  editPromotorEvent: (req, res) => {
-
+  editPromotorEvent: async (req, res, next) => {
+    try {
+      const updateEvent = await events.update({ where: { id: req.params.id } });
+    } catch (error) {}
   },
 
   deletePromotorEvent: async (req, res, next) => {
     console.log("masuk delete");
-    console.log("req params,", req.params); 
+    console.log("req params,", req.params);
     try {
       const eventDelete = await events.destroy({
         where: {
-          id: req.params.id
-        }
-      })
+          id: req.params.id,
+        },
+      });
       const ticketDelete = await tickets.destroy({
         where: {
-          idEvent: req.params.id
-        }
-      })
+          idEvent: req.params.id,
+        },
+      });
       const promoDelete = await promos.destroy({
         where: {
-          idEvent: req.params.id
-        }
-      })
-      
-      next(templateRes(200, true, "Delete Event Success", eventDelete, null))
+          idEvent: req.params.id,
+        },
+      });
+
+      next(templateRes(200, true, "Delete Event Success", eventDelete, null));
     } catch (error) {
       console.log(error);
-      next(templateRes(200, true, "Delete Event UnSuccessfull", null, error.message))
+      next(templateRes(200, true, "Delete Event UnSuccessfull", null, error.message));
     }
   },
 
   postPromotorEvent: async (req, res, next) => {
     try {
-
       const result = await events.create({
         name: req.body.name,
         id_city: req.body.id_city,
@@ -211,32 +235,22 @@ module.exports = {
         picture: req.file.filename,
         id_category: req.body.id_category,
         id_promotor: req.body.id_promotor,
-      })
+      });
 
-      return next(templateRes(200, true, "Success create event", result, null))
+      return next(templateRes(200, true, "Success create event", result, null));
     } catch (error) {
-      fs.unlinkSync(`${req.file.path}`)
-      next(templateRes(500, false, "error create event", null, error.message))
+      fs.unlinkSync(`${req.file.path}`);
+      next(templateRes(500, false, "error create event", null, error.message));
     }
   },
 
-  getPromotorTicket: (req, res) => {
+  getPromotorTicket: (req, res) => {},
 
-  },
+  postPromotorTicket: (req, res) => {},
 
-  postPromotorTicket: (req, res) => {
+  editPromotorTicket: (req, res) => {},
 
-  },
+  deletePromotorTicket: (req, res) => {},
 
-  editPromotorTicket: (req, res) => {
-
-  },
-
-  deletePromotorTicket: (req, res) => {
-
-  },
-
-  postPromotorTicket: (req, res) => {
-
-  }
-}
+  postPromotorTicket: (req, res) => {},
+};
--- Routes/promotor.js
+++ Routes/promotor.js
@@ -1,29 +1,26 @@
-const {promotorController} = require("../Controller");
-const {uploader} = require("../Helper/uploader");
-const { validateTokenPromotor } = require("../middleware/validatePromotor");
+const { promotorController } = require("../Controller");
+const { uploader } = require("../Helper/uploader");
 const { validateToken } = require("../middleware/validation");
 const router = require("express").Router();
 
 // Promotor Publish
-router.post("/publish",validateToken,promotorController.publish)
-
+router.post("/publish", validateToken, promotorController.publish);
 
 // Routing untuk landing login promotor
-router.post("/", promotorController.dummyLogin)
-router.get("/checkrole",validateTokenPromotor, promotorController.checkRole)
+// router.post("/", promotorController.dummyLogin);
 
 // Routing untuk manage event
-router.get("/manage-event", validateToken,promotorController.getPromotorEvent)
-router.patch("/manage-event", promotorController.editPromotorEvent)
-router.delete("/manage-event/:id",validateToken, promotorController.deletePromotorEvent)
+router.get("/manage-event", validateToken, promotorController.getPromotorEvent);
+router.patch("/manage-event", validateToken, promotorController.editPromotorEvent);
+router.delete("/manage-event/:id", validateToken, promotorController.deletePromotorEvent);
 
 // Routing untuk create event
-router.post("/create-event",promotorController.publish)
+router.post("/create-event", promotorController.publish);
 
 // Routing untuk  mendapatkan category
 // router.get("/get-category",promotorController.getCategory)
 
 // Routing untuk upload image ke server
-router.post("/create/event",uploader("/event_image").single("fileUpload"))
+router.post("/create/event", uploader("/event_image").single("fileUpload"));
 
 module.exports = router;
